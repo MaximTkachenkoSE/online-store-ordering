1bbbe0b594d21a29ebd9569d9c7e59a6be28805c
Maxim Tkachenko <maksimtkacenko82@gmail.com>
2022-05-12 11:10:44 +0300

    git commit

---
+++ Ordering.js
@@ -0,0 +1,12 @@
+// class OrderingPage {
+//     render() {
+//         const html = `
+        
+//         `;
+
+//         document.getElementById("ordering").innerHTML = html;
+//     }
+// }
+
+// const orderingPage = new OrderingPage;
+// orderingPage.render();
